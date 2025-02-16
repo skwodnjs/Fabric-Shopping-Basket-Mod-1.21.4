7763c42ebbc4d368a767ca50c76f7578e45c5bde
JWN <naa3000@naver.com>
2025-02-17 01:35:06 +0900

    first commit

---
+++ src/main/java/jwn/shopping_basket_mod/ShoppingBasketModClient.java
@@ -0,0 +1,10 @@
+package jwn.shopping_basket_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShoppingBasketModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
